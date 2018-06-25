713f75a8800d67d20811a6887c2db6b002829b9d
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-25 23:21:14 +0100

    first commit

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,20 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Jessica Stenning on 25/06/2018.
+//  Copyright © 2018 Jessica Stenning. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        if number % 3 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,41 @@
+//
+//  BrainTests.swift
+//  FizzBuzzTests
+//
+//  Created by Jessica Stenning on 25/06/2018.
+//  Copyright © 2018 Jessica Stenning. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByThree() {
+        let brain = Brain ()
+        let result = brain.isDivisibleByThree(number: 1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive() {
+        let brain = Brain()
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+}
