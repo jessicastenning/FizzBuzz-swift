b58b46da2a6222ed7a1af3e6deceda20eaa07051
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-26 08:18:37 +0100

    Adds game class with score

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Jessica Stenning on 26/06/2018.
+//  Copyright © 2018 Jessica Stenning. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+ 
+    var score: Int
+    
+    init() {
+        score = 0
+    }
+}
